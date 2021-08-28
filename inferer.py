ae8af67ab2fab50b1d5479841276b44a76ac96e1
PhiDCH <phi9b2@gmail.com>
2021-08-29 00:38:48 +0700

    adasd

--- inferer.py
+++ inferer.py
@@ -18,6 +18,29 @@ from torch.utils.data import Dataset, DataLoader
 import cv2 
 import numpy as np
 
+def crop_with_padding(img, pts):
+    pts_ = pts.reshape((-1, 2)).astype(int)
+
+    rect = cv2.boundingRect(pts_)
+    x,y,w,h = rect
+    croped = img[y:y+h, x:x+w].copy()
+
+    ## (2) make mask
+    pts_ = pts_ - pts_.min(axis=0)
+
+    mask = np.zeros(croped.shape[:2], np.uint8)
+    cv2.drawContours(mask, [pts_], -1, (255, 255, 255), -1, cv2.LINE_AA)
+
+    ## (3) do bit-op
+    dst = cv2.bitwise_and(croped, croped, mask=mask)
+
+    ## (4) add the white background
+    bg = np.ones_like(croped, np.uint8)*255
+    cv2.bitwise_not(bg,bg, mask=mask)
+    dst2 = bg+ dst
+
+    return dst2
+
 def text_recog(img, boxes, recog_model):
     temp = {'boxes': None,'pil_img': None, 'text': None}
     result = []
